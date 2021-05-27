9f42540e438fa8b85fb6a2b152f8326e0ef4843a
davidepatti <xedivad@gmail.com>
2021-05-27 16:01:11 +0200

    Added check for args

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        if (args.length!=2) {
+            System.out.println("nooo! this program requires two args");
+        }
     }
 }
